0c6231e33e2e18ae7ced56c5877054c8cef94457
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-23 23:16:41 +0530

    .

--- src/main/java/org/vimal/security/v1/util/aes/DeterministicAESUtil.java
+++ src/main/java/org/vimal/security/v1/util/aes/DeterministicAESUtil.java
@@ -3,22 +3,29 @@ package org.vimal.security.v1.util.aes;
 import org.vimal.security.v1.util.aes.key.AESKeyProvider;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.util.Arrays;
+import java.security.MessageDigest;
 import java.util.Base64;
 
 public class DeterministicAESUtil implements AESInterface {
     private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
     private static final byte[] FIXED_IV = new byte[16];
-    private final SecretKeySpec keySpec;
-
-    static {
-        Arrays.fill(FIXED_IV, (byte) 0x1A);
-    }
+    private final SecretKey secretKey;
 
     public DeterministicAESUtil(AESKeyProvider aesKeyProvider) {
-        this.keySpec = new SecretKeySpec(aesKeyProvider.getAesSecret().getBytes(), "AES");
+        this.secretKey = deriveKey(aesKeyProvider.getAesSecret());
+    }
+
+    private SecretKey deriveKey(String secret) {
+        try {
+            var sha256 = MessageDigest.getInstance("SHA-256");
+            var keyBytes = sha256.digest(secret.getBytes());
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (Exception e) {
+            throw new RuntimeException("Error occurred while deriving AES key in DeterministicAESUtil: " + e.getMessage(), e);
+        }
     }
 
     @Override
@@ -26,7 +33,7 @@ public class DeterministicAESUtil implements AESInterface {
         try {
             var cipher = Cipher.getInstance(TRANSFORMATION);
             var ivSpec = new IvParameterSpec(FIXED_IV);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
             var encryptedBytes = cipher.doFinal(data.getBytes());
             return Base64.getEncoder().encodeToString(encryptedBytes);
         } catch (Exception e) {
@@ -39,7 +46,7 @@ public class DeterministicAESUtil implements AESInterface {
         try {
             var cipher = Cipher.getInstance(TRANSFORMATION);
             var ivSpec = new IvParameterSpec(FIXED_IV);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
             var decodedBytes = Base64.getDecoder().decode(encryptedData);
             return new String(cipher.doFinal(decodedBytes));
         } catch (Exception e) {
