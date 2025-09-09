d3d329ca94fbccc93ef9cb8fb1f3693d9d53ae93
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-09 10:07:33 +0530

    .

--- src/main/java/org/vimal/security/v1/V1Application.java
+++ src/main/java/org/vimal/security/v1/V1Application.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import java.util.TimeZone;
+
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-public class V1_3Application {
+public class V1Application {
     public static void main(String[] args) {
-        SpringApplication.run(V1_3Application.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(V1Application.class, args);
     }
 }
